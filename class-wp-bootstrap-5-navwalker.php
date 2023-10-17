44b616d6e9ed3a813bcc95b40ca6d406e2ed7d5e
Alexandre Kozoubsky <akozoubsky@gmail.com>
2023-10-17 04:00:05 -0300

    Update class-wp-bootstrap-5-navwalker.php
    nav-item -> nav-link

--- class-wp-bootstrap-5-navwalker.php
+++ class-wp-bootstrap-5-navwalker.php
@@ -175,7 +175,9 @@ if ( ! class_exists( 'WP_Bootstrap_5_Navwalker' ) ) :
 
 			// Add some additional default classes to the item.
 			$classes[] = 'menu-item-' . $item->ID;
-			$classes[] = 'nav-item';
+			//$classes[] = 'nav-item';
+			// Code has been updated for Bootstrap 5
+			$classes[] = 'nav-link';
 
 			// Allow filtering the classes.
 			$classes = apply_filters( 'nav_menu_css_class', array_filter( $classes ), $item, $args, $depth );
@@ -376,8 +378,11 @@ if ( ! class_exists( 'WP_Bootstrap_5_Navwalker' ) ) :
 			$class            = $args['menu_class'] ? ' class="menu-fallback-menu ' . esc_attr( $args['menu_class'] ) . '"' : ' class="menu-fallback-menu"';
 			$id               = $args['menu_id'] ? ' id="' . esc_attr( $args['menu_id'] ) . '"' : '';
 			$fallback_output .= '<ul' . $id . $class . '>';
-			$fallback_output .= '<li class="nav-item"><a href="' . esc_url( admin_url( 'nav-menus.php' ) ) . '" class="nav-link" title="' . esc_attr__( 'Add a menu', 'wp-bootstrap-navwalker' ) . '">' . esc_html__( 'Add a menu', 'wp-bootstrap-navwalker' ) . '</a></li>';
+			//$fallback_output .= '<li class="nav-item"><a href="' . esc_url( admin_url( 'nav-menus.php' ) ) . '" class="nav-link" title="' . esc_attr__( 'Add a menu', 'wp-bootstrap-navwalker' ) . '">' . esc_html__( 'Add a menu', 'wp-bootstrap-navwalker' ) . '</a></li>';
+			// Code has been updated for Bootstrap 5
+			$fallback_output .= '<li class="nav-link"><a href="' . esc_url( admin_url( 'nav-menus.php' ) ) . '" class="nav-link" title="' . esc_attr__( 'Add a menu', 'wp-bootstrap-navwalker' ) . '">' . esc_html__( 'Add a menu', 'wp-bootstrap-navwalker' ) . '</a></li>';
 			$fallback_output .= '</ul>';
+			
 
 			// Menu container closing tag.
 			if ( $show_container ) {
